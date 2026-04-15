baf909786bfdd1a11f49c7f36309bbf23d8512d9
Grayholm <rodiongrayholm@gmail.com>
2026-04-16 01:31:42 +0300

    Окончательная форма

--- src/db.py
+++ src/db.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
-DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost:6432/short_links"
+DATABASE_URL = "postgresql+asyncpg://postgres:postgres@db:5432/short_links"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session: async_sessionmaker = async_sessionmaker(bind=engine, expire_on_commit=False)
